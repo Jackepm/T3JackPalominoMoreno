ef85117e6134fe214c26c82f90c5432774687131
Jackemp <jackpaalomino@gmail.com>
2024-11-14 08:14:36 -0500

    Clase Cita

--- Gestiondecitasmedicas/Cita.cs
+++ Gestiondecitasmedicas/Cita.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Gestiondecitasmedicas
 {
-    internal class Cita
+    public class Cita
     {
+        public int numero { get; set; }
+        public Estudiante estudiante { get; set; }
+        public string enfermedad { get; set; }
+        public double precio { get; set; }
+
+        public Cita(int numero, Estudiante estudiante, string enfermedad, double precio)
+        {
+            this.numero = numero;
+            this.estudiante = estudiante;
+            this.enfermedad = enfermedad;
+            this.precio = precio;
+        }
+
+        public override string ToString()
+        {
+            return $"Número: {numero}, Estudiante: {estudiante}, Enfermedad: {enfermedad}, Precio: {precio}";
+        }
     }
 }
