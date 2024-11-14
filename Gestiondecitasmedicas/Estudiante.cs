b86b17961ae1116b9a2473e81ac0748d23303e11
Jackemp <jackpaalomino@gmail.com>
2024-11-14 08:11:10 -0500

    Clase Estudiante

---
+++ Gestiondecitasmedicas/Estudiante.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestiondecitasmedicas
+{
+    public class Estudiante
+    {
+        public int codigo { get; set; }
+        public string nombre { get; set; }
+        public string universidad { get; set; }
+
+        public Estudiante(int codigo, string nombre, string universidad)
+        {
+            this.codigo = codigo;
+            this.nombre = nombre;
+            this.universidad = universidad;
+        }
+
+        public override string ToString()
+        {
+            return $"Código: {codigo}, Nombre: {nombre}, Universidad: {universidad}";
+        }
+    }
+}
