2d33c990ac201930cc621ec98b9b0be1db0321b3
Jackemp <jackpaalomino@gmail.com>
2024-11-14 08:21:28 -0500

    Listado para programa

--- Gestiondecitasmedicas/Program.cs
+++ Gestiondecitasmedicas/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Crear Cita");
+            Console.WriteLine("2. Listar Citas");
+            Console.WriteLine("3. Modificar Masivo");
+            Console.WriteLine("4. Fin");
+            Console.Write("Elige una opción: ");
+            string opcion = Console.ReadLine();
+
+            switch (opcion)
+            {
+                case "1":
+                    
+                    break;
+                case "2":
+                    
+                    break;
+                case "3":
+                    
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+        }
+    }
+}
